97a15276b02747e35ac2ae39617872579f715f83
codewithfaizan <faizanoic.com@gmail.com>
2022-07-18 10:57:50 +0530

    added

--- 5.js
+++ 5.js
@@ -1,49 +1,63 @@
-// Arithematic operater 
+// // Arithematic operater 
 
-a = 2 , b = 4;
-console.log(a+b)
+// a = 2 , b = 4;
+// console.log(a+b)
 
-console.log(4+b)
-
-
-console.log(a+11)
-
-console.log(a-b)
-
-console.log(a+b,a-b,6*8)
-
-console.log(a/b)
-
-console.log(a/b/0) // not an error , it's output
-
-console.log(a*b*0)
-
-console.log(a*b+a/2-2)
+// console.log(4+b)
 
 
-console.log(a*(b+a)/2-2)
+// console.log(a+11)
 
-console.log(a+(a-(b*a)))
+// console.log(a-b)
 
-console.log(a%b)
-//divisor =b ; dividend =a
-// 4)2( 
+// console.log(a+b,a-b,6*8)
 
-    console.log(a/b)
+// console.log(a/b)
 
-// float divison a//b 
+// console.log(a/b/0) // not an error , it's output
 
-console.log(Math.floor(0.9))
+// console.log(a*b*0)
 
-console.log(Math.ceil(0.9)) 
+// console.log(a*b+a/2-2)
 
-console.log(Math.round(0.9))
 
-console.log(4**3)
+// console.log(a*(b+a)/2-2)
 
-console.log((8**2)**2)
+// console.log(a+(a-(b*a)))
 
-console.log(3**2**3) 
+// console.log(a%b)
+// //divisor =b ; dividend =a
+// // 4)2( 
+
+//     console.log(a/b)
+
+// // float divison a//b 
+
+// console.log(Math.floor(0.9))
+
+// console.log(Math.ceil(0.9)) 
+
+// console.log(Math.round(0.9))
+
+// console.log(4**3)
+
+// console.log((8**2)**2)
+
+// console.log(3**2**3) 
+
+
+// console.log(4**2) 
+
+console.log(4**2**3)
+
+
+console.log(4**(2**3))
+
+
+console.log((4**2)**3) 
+
+console.log() 
+
 
 
 
---
+++ 6.js
@@ -0,0 +1,49 @@
+// Assignment operator
+
+// var a = 3,b =4, c =6
+// console.log(a)
+
+// a = a+2 // a = a+2
+// console.log(a)
+
+// b+=4 // b = b+4 ; 4+4
+// c+=3 // c = c+3 ; 3+3
+// console.log(b)
+// console.log(c)
+
+// a =a+b
+// b+=c
+// c+=a
+// console.log(c,a,b)
+
+// a+=b
+// a-=2 
+// b*=c
+// c-=b
+// console.log(b,a,c)
+ 
+// Comparision Operator
+// a+=b-c
+// b-=b+=c
+// console.log(b,a,c)
+// var a=5,b=-3,c=-6
+// a%=b
+// console.log(a)
+// console.log(c+',')
+// console.log(a<b)
+// console.log(b>b)
+// console.log(a>b)
+// console.log(c<b<c)
+// console.log(c<b<a)
+// console.log(a>b)
+// console.log(a>b)
+
+var a=5,b=-3,c=-6
+
+console.log(c>b>c) //true
+console.log(c<b<a) //true
+console.log(c>b<a) //true
+console.log(c>b<a) //true
+console.log(a>b<c) //false
+console.log(b<a<c) //false
+console.log(c<c<c) //
