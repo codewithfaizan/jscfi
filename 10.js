405addbfb3daa20a2a1d8f1670ee38b8f0da73c7
codewithfaizan <faizanoic.com@gmail.com>
2022-07-21 08:21:29 +0530

    added file

---
+++ 10.js
@@ -0,0 +1,24 @@
+// console.log('1'*'2')
+// console.log('1'*2)
+// console.log(1*'2')
+// console.log(100 *1 +'2')
+
+
+// console.log('100'*null)
+// console.log(100+null)
+// console.log(8*null) 
+
+// console.log(1/0) // inf
+// console.log(1 / Infinity) // 0
+// console.log(Infinity/Infinity) //1
+// console.log(-Infinity/0)
+// console.log(Infinity/0) // 0
+
+// console.log(NaN == NaN)
+// console.log(typeof NaN)
+// console.log(NaN === NaN)
+// console.log(infinity/infinity)
+// console.log(typeof Infinity);
+
+var value = Infinity;
+console.log(typeof value);
