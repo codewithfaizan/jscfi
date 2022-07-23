d3530c1a9b721196e50158d91031ecae14f80cab
codewithfaizan <faizanoic.com@gmail.com>
2022-07-23 10:11:27 +0530

    folder vlc added with v, l, c

---
+++ vlc.js/const.js
@@ -0,0 +1,26 @@
+// const a =4; //declared
+// a =3;
+// console.log(a)
+
+// var a =3;
+// const a =4;
+// cosnsole.log(a) //cAannot n=be reinitialised
+
+// const a =4 ;
+// console.log(a)
+
+// const a=2;
+// console.log(a);
+// {
+//     const a = 12;
+//     console.log(a);
+// }
+// console.log(a);
+
+
+// const a =12;
+// console.log(a);
+// {
+//     let a = 2;
+//     console.log()
+// }
---
+++ vlc.js/let.js
@@ -0,0 +1,43 @@
+// let a ; //declare
+// a=6; //define
+// console.log(a);
+
+// a =3; // define
+// let a =4; //declare
+// console.log(a); // not possible bcz cannot access a before initial
+
+
+//let trials
+// let a =1;
+// console.log(a);
+
+// let a =2;
+// console.log(a) //
+
+
+// let a = 4;
+// console.log(a);
+// {
+//     let a =5;
+//     console.log(a);
+// }
+  
+// let a =3; // local 
+// console.log(a);
+// {
+//     let a = 4; //local
+//     console.log(a);
+// }
+// console.log(a); // value of a is not updated bcz its value is same as in the gloal scope
+
+
+// let a=3;
+// console.log(a);
+// {
+//     a=4;
+//     console.log(a);
+// }
+// var a=7;;
+// console.log(a);  /// not possible, cannot be modified
+
+
