ca97d29e9eb846e95de19100a0b2b1058000d51f
codewithfaizan <faizanoic.com@gmail.com>
2022-07-27 09:15:24 +0530

    added files

---
+++ Iterations/whileloop.js
@@ -0,0 +1,57 @@
+// Initialization (Start value)
+// wwile (Condition till it terminates-false){
+        //  task/ Statement
+// }
+
+//------------program to print 1-10----------
+// i=1                         
+// while(i<=10){              
+//     console.log(i)
+    // i++
+    // i = i+1
+//     i+=1
+// }
+
+
+//iteration-1 : i = 1; 1<=10; 1 ; (2)
+//iteration-2 : i = 2; 2<=0;  2 ; (3)
+//iteration-3 : i = 3; 3<=10; 3 ; (4)
+//iteration-4 : i = 4; 4<=10; 4 ; (5)
+//iteration-5 : i = 5; 5<=10; 5 ; (6)
+//iteration-6 : i = 6; 6<=10; 6 ; (7)
+//iteration-7 : i = 7; 7<=10; 7 ; (8)
+//iteration-8 : i = 8; 8<=10; 8 ; (9)
+//iteration-9 : i = 9; 9<=10; 9 ; (10)
+
+
+//-----------------------------program to print 10-1----------
+// i=10                     
+// while(i>=1){
+//     console.log(i);
+//     i--;
+    // i = i+1
+    // i-=1
+// }
+
+//-------program ton print even number starting from 50 - 100
+//  i = 50;
+//  while(i<=100){
+//         console.log(i)
+//     i=i+2
+//  }
+
+//---------program to print odd number starting from 50-100
+// i = 51;
+// while(i<=100){
+//     console.log(i)
+//     i=i+2
+// }
+
+//-----------even number using logic-------
+i = 50
+while(i<=100){
+    if ((i%2)==0){
+        console.log(i);     
+    } 
+    i+2; 
+}
