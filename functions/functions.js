d54f659b9fa41d56d441c0578ba71f97489dc0d2
codewithfaizan <faizanoic.com@gmail.com>
2022-08-01 17:38:37 +0000

    added in jscfi

---
+++ functions/functions.js
@@ -0,0 +1,46 @@
+// practise 
+// ASCII to character
+// String.fromCharCode(68) //this is a method that returns a string created from 
+// the specified sequence of UTf-16 codes
+// console.log(String.fromCharCode(68,67,66,65));
+// console.log(String.fromCharCode(70,65,73,90,65,78));
+
+
+// let a ="FAIZAN"
+// am = a.charCodeAt(2) // this method returns the unicode of the character
+// at a specified index position in a string
+// console.log(am);
+
+//function ----------------------
+
+// A function is a block of code made to perform a particular task. It is executed only when it is invoked or called
+// function name(parameter){   // defined with the function keyword followed by the name followed by parantheses()
+    //statement 
+// }
+// name(args)
+
+// function mult(a,b){
+//     console.log(a*b)
+//     return;
+//     console.log(a+b); // code to be executed
+// }
+// mult(2,4)
+
+// let x = myfunction(10,10)
+
+// function myfunction(a,b){
+//     console.log(a/b);
+//     return;
+//     console.log(a*b);
+// }
+
+// function is declared once, define code once and the code is used many times and with different arguments
+
+// function toCelsius(fahrenheit) {
+//     console.log(5/9)*(fahrenheit-32)
+// }
+// toCelsius()
+
+//function can be used a variable value 
+
+
