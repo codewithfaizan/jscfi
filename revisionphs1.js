7d850b06942906cc9f6d1e7b0f19810863140980
codewithfaizan <faizanoic.com@gmail.com>
2022-08-04 19:01:41 +0000

    revision

---
+++ revisionphs1.js
@@ -0,0 +1,50 @@
+// revision session for phase1 test on 05.08.2022 // gmeet 04.08.2022 @7.30pm // 
+// 7.30 to 9.30 ---- basics revision
+// 10.00 to 1.00 ---- main topics 
+
+//Basics ---------------------------------
+    //variables, which are valid and which are not valid--done
+    // commenting -- single line comment nad double line comment 
+     //Operators
+
+     console.log(Math.floor(0/2));
+    //  console.log(Math.ceil(4/3));
+    //  a =a+4 is same as a+=4 ie a = a+4
+    // let a = 10 
+    // console.log(a*=2);
+
+    // console.log(3==3);
+    // console.log('hello'==='Hello');
+    // console.log(2==='2');
+
+    // precedence 
+    // console.log(0x111); //to decimal conversion
+    // a = 12
+    // console.log(a.toString(2));
+    // console.log(a.toString(16));
+//   console.log(+'t');
+//   console.log(-"t");
+
+// console.log(typeof NaN);
+
+// typeconversion
+// concatentation
+
+// string
+// var a = 'smart'
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[4]);
+// console.log(a.length);
+// console.log(a.slice(1));
+// console.log(a.splice(2));
+
+// 
+// var xyz = 2;
+// console.log(xyz);
+// {
+    // var xyx = 5;
+// console.log(xyz);
+// }
+// console.log(xyz);
+// var xyz = 2;
