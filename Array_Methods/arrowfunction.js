6b547193a29fe247974d0a2fda889ee91c4e168f
codewithfaizan <faizanoic.com@gmail.com>
2022-08-22 18:16:04 +0000

    updated

---
+++ Array_Methods/arrowfunction.js
@@ -0,0 +1,15 @@
+let a = fruit => console.log('fruit');
+
+// console.log(a)
+
+
+// arrow function and map 
+// function double (number){
+//     return number*2
+// }
+// console.log(double(2))
+
+
+const double = number => number*2;
+console.log(double(2))
+
